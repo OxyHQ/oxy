ec63f890f060f5d4767672e210966a0e2e05c5a2
Nate Isern <nate.isern.alvarez@gmail.com>
2026-09-10 23:20:25 +0300

    chore(theme): adopt Bloom document theme contract

--- packages/app-preset/__tests__/package-metadata.test.js
+++ packages/app-preset/__tests__/package-metadata.test.js
@@ -3,6 +3,6 @@ const test = require('node:test');
 const packageJson = require('../package.json');
 
 test('Oxy peers accept compatible patches without crossing a major', () => {
-  assert.equal(packageJson.peerDependencies['@oxy.so/bloom'], '^1.0.0');
+  assert.equal(packageJson.peerDependencies['@oxy.so/bloom'], '^1.0.7');
   assert.equal(packageJson.peerDependencies['@oxy.so/services'], '^1.0.1');
 });
